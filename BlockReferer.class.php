4e17834eb62c92358d4f2c9efc3de31eded1d651
Nuno Barreto <nbarreto@gmail.com>
2014-07-24 12:51:58 +0000

    correct fake referrer for redirects

--- BlockReferer.class.php
+++ BlockReferer.class.php
@@ -203,8 +203,15 @@ class BlockReferer {
      * @param string $fakeReferer Fake Referer.
      * @param string $userAgent User Agent to use. Browser user agent prefered.
      */
-    function fakeReferer($targetUrl, $fakeReferer, $userAgent) {
-        $curl = curl_init();
+    function fakeReferer($targetUrl='', $fakeReferer='', $userAgent='') {
+        if ($targetUrl == '') {
+            $targetUrl = self::getTargetUrl();
+        }
+        if ($userAgent == '') {
+            $userAgent = 'Googlebot/2.1 (+http://www.google.com/bot.html)';
+        }
+
+        $ch = curl_init();
         $header[0]  = "Accept: text/xml,application/xml,application/xhtml+xml,";
         $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
         $header[]   = "Cache-Control: max-age=0";
@@ -214,24 +221,33 @@ class BlockReferer {
         $header[]   = "Accept-Language: en-us,en;q=0.5";
         $header[]   = "Pragma: "; // browsers keep this blank.
 
-        curl_setopt($curl, CURLOPT_URL, $targetUrl);
-        curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($curl, CURLOPT_REFERER, $fakeReferer);
-        curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
-        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 5000);
+        curl_setopt($ch, CURLOPT_URL, $targetUrl);
+        curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_REFERER, $fakeReferer);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5000);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
 
-        // @todo: follow redirects
-        // @todo: see http://stackoverflow.com/questions/3519939/make-curl-follow-redirects
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        $response = curl_exec($ch);
 
-        $html = curl_exec($curl);
-        curl_close($curl);
+
+        $baseUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+        $body = substr($response, $headerSize);
+
+        $body = substr($body, 0, strpos($body, '<head>') + 6).
+                '<base href="'.$baseUrl.'">'.
+                substr($body, strpos($body, '<head>') + 6);
+
+        curl_close($ch);
 
         // returns the content provided by the site
-        echo $html;
+        echo $body;
     }
 }
 
--- README.md
+++ README.md
@@ -3,12 +3,23 @@ block-referer-php
 
 A PHP lib for blocking referer
 
-Usage:
+Blank Referer Usage:
 
     <?php
     require_once('BlockReferer.class.php');
 
     BlockReferer::allBlankMethods();
+    ?>
+
+Fake Referer Usage:
+
+    <?php
+    require_once('../BlockReferer.class.php');
+
+    $referer = 'http://www.example.com';
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+    BlockReferer::fakeReferer('', $referer, $userAgent);
     ?>
 
 Example:
